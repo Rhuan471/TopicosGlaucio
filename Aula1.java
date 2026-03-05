800210037d35d0c5c1506ba4cf399899b973c4c2
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2026-03-04 21:23:44 -0300

    Add aula1 class extending Thread
    Implement a thread class 'aula1' that prints numbers.

---
+++ Aula1.java
@@ -0,0 +1,21 @@
+public class aula1 extends Thread {
+    private int id;
+
+    public aula1(int id) {
+        this.id = id;
+    }
+    
+    public void run(){
+        for (int i = 1; i<=10; i++){
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x++){
+        aula1 a1 = new aula1(x);
+        a1.start();
+        }
+        System.out.println("ok");
+}
+}
