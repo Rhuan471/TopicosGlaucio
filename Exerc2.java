b6cda687dda192c65c0169c7d8a07758c01ad142
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2026-03-11 20:59:26 -0300

    Update Exerc2.java

--- Exerc2.java
+++ Exerc2.java
@@ -1,3 +1,5 @@
+// Faça um programa concorrente que crie 10 threads que exibam o nome da thread e um contador que varia de 1 a 10. Execute várias 
+// vezes e observe as variações na saída. Os valores são sempre impressos na mesma ordem?
 public class Exerc2 extends Thread {
     public void run() {
         for (int i = 1; i <= 10; i++) {
