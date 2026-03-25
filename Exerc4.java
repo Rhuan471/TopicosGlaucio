35856b18636e9097dbac4ccf851a0acd91154238
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2026-03-25 19:58:58 -0300

    Refactor threading and list initialization
    Refactored threading implementation to use new Thread.ofPlatform() method and simplified list initialization.

--- Exerc4.java
+++ Exerc4.java
@@ -6,56 +6,10 @@ import java.util.Random;
 public class Exerc4 {
 
     public static void main(String[] args) {
-        List<Integer> lista = Collections.synchronizedList(new ArrayList<>());
-        Random random = new Random();
-
-        Thread produtor1 = new Thread(() -> {
-            while (true) {
-                lista.add(random.nextInt(100));
-                pausar(300); 
-            }
-        });
-
-        Thread produtor2 = new Thread(() -> {
-            while (true) {
-                lista.add(random.nextInt(100));
-                pausar(400);
-            }
-        });
-
-        Thread consumidor = new Thread(() -> {
-            while (true) {
-
-                synchronized (lista) {
-                    if (!lista.isEmpty()) {
-                        lista.remove(0);
-                    }
-                }
-                pausar(500); 
-            }
-        });
-
-        Thread monitor = new Thread(() -> {
-            while (true) {
-                pausar(1000); 
-                
-                synchronized (lista) {
-                    System.out.println("Lista atual: " + lista);
-                }
-            }
-        });
-
-        produtor1.start();
-        produtor2.start();
-        consumidor.start();
-        monitor.start();
-    }
-
-    private static void pausar(int milissegundos) {
-        try {
-            Thread.sleep(milissegundos);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        List<Integer> lista = new Arraylist<>();
+        Thread.ofPlataform().start(new Incluir(lista));
+        Thread.ofPlataform().start(new Incluir(lista));
+        Thread.ofPlataform().start(new Remover(lista));
+        Thread.ofPlataform().start(new Imprimir(lista));
     }
 }
